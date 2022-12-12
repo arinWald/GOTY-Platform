39c78cf9136564f18fb0a926cc0e97c7857148aa
xaita <99950138+xaita@users.noreply.github.com>
2022-12-12 19:06:37 +0100

    a

--- Game/Source/FlyingEnemy.h
+++ Game/Source/FlyingEnemy.h
@@ -1,1 +1,106 @@
-#pragma once
+#ifndef __FLYINGENEMY_H__
+#define __FLYINGENEMYR_H__
+
+#include "Entity.h"
+#include "Point.h"
+#include "Animation.h"
+#include "Physics.h"
+#include "SDL/include/SDL.h"
+#include "Animation.h"
+
+struct SDL_Texture;
+
+struct transformPosition {
+	float posX;
+	float posY;
+	bool turn;
+
+};
+
+class Player : public Entity
+{
+public:
+
+	Player();
+
+	virtual ~Player();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update();
+
+	bool CleanUp();
+
+	// L07 DONE 6: Define OnCollision function for the player. Check the virtual function on Entity class
+	void OnCollision(PhysBody* physA, PhysBody* physB);
+
+	void Player::ChangePosition(int x, int y);
+
+
+public:
+
+	//L02: DONE 1: Declare player parameters
+	SDL_Texture* playerTexture;
+	SDL_Texture* playerLivesTexture;
+	const char* livesTexturePath;
+	const char* texturePath;
+	const char* deathFxPath;
+	const char* jumpFxPath;
+	const char* level1SongPath;
+	const char* defeatFxPath;
+
+	Animation* currentAnimation;
+	Animation rightRunAnimation;
+	Animation rightIdleAnimation;
+	Animation rightJumpAnimation;
+	Animation rightFallAnimation;
+	Animation rightDoubleJumpAnimation;
+
+	Animation leftRunAnimation;
+	Animation leftIdleAnimation;
+	Animation leftJumpAnimation;
+	Animation leftFallAnimation;
+	Animation leftDoubleJumpAnimation;
+
+	Animation dissappearAnimation;
+
+	int speed;
+	int jumpspeed;
+	bool ground;
+	int jumpsavailable;
+	int playerlives;
+
+	// L07 DONE 5: Add physics to the player - declare a Physics body
+	PhysBody* pbody;
+	PhysBody* groundSensor;
+
+	int timerJump;
+	int pickCoinFxId;
+
+	int timerPocho;
+	int deathFxId;
+
+	int defeatFxId;
+
+	int timerDeath;
+	bool isDead;
+
+	bool isWin;
+
+	int jumpFxId;
+	int LastDir;
+
+	transformPosition teleport;
+
+	int initialPosX;
+	int initialPosY;
+
+private:
+
+
+
+};
+
+#endif
